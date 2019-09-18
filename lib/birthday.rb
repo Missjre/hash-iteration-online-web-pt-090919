7bf13cbf65c477e48282791355369bf535291347
Jalysa Reason <jalysareason@gmail.com>
2019-09-18 01:17:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,13 @@
 # }
 
 def happy_birthday(birthday_kids)
-  
+    birthday_kids = {
+      "Timmy" => 9,
+      "Sarah" => 6,
+      "Amanda" => 27
+    }
+
+    puts {happy_birthday(birthday_kids)}=("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nHappy Birthday Amanda! You are now 27 years old!\n").to_stdout
 end
 
 
